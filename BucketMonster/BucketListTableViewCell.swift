256694c5862a850e7917fd546fe05c29212b1692
Christopher Reed <ctr5qp@virginia.edu>
2017-02-11 00:49:25 -0500

    Add the cell view with connections to BucketListTableViewCell.swift

---
+++ BucketMonster/BucketListTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BucketListTableViewCell.swift
+//  BucketMonster
+//
+//  Created by Christopher Reed on 2/11/17.
+//  Copyright © 2017 Christopher Reed. All rights reserved.
+//
+
+import UIKit
+
+class BucketListTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var bucketItemLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
