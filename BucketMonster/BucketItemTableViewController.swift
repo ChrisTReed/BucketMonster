d0220d7212339d3a91986208fb0851a33df2aa47
Christopher Reed <ctr5qp@virginia.edu>
2017-02-20 18:24:42 -0500

    Allow sorting of list based on completion.  Change the color of correct cells corresponding to finished items.  Fix issue in returning previously completed items to an unmarked color after being edited.

--- BucketMonster/BucketItemTableViewController.swift
+++ BucketMonster/BucketItemTableViewController.swift
@@ -27,7 +27,7 @@ class BucketItemTableViewController: UITableViewController {
     private func loadSampleData() {
         
         let b1 = BucketItem(name: "Wyprawa do Austalii", desc: "yeet", lat: 37.8, lon: 144.9, date: Date(), done: false)
-        let b2 = BucketItem(name: "Żaglowanie gdzieś na Karaibach", desc: "yeet", lat: 21.4, lon: 79.6, date: Date(), done: false)
+        let b2 = BucketItem(name: "Żaglowanie gdzieś na Karaibach", desc: "yeet", lat: 21.4, lon: 79.6, date: Date(), done: true)
         let b3 = BucketItem(name: "Opublikowanie artykułu", desc: "yeet", lat: 50.0, lon: 19.9, date: Date(), done: false)
 
         bucketList += [b1, b2, b3]
@@ -43,33 +43,33 @@ class BucketItemTableViewController: UITableViewController {
         //Organizing bucket by completion then date
         //http://stackoverflow.com/questions/29530620/sort-array-of-objects-by-two-properties
      
-        bucketList.sort {
-            if !$0.done && $1.done {
-                return true
-            }
-            if $0.done && !$1.done {
-               return false
-            }
-            if $0.done == $1.done {
-                return $0.date < $1.date
-            }
-            
-            return false
-        }
-        
-//        bucketList.sort { (s1, s2) -> Bool in
-//            if !s1.done && s2.done {
+//        bucketList.sort {
+//            if !$0.done && $1.done {
 //                return true
 //            }
-//            if s1.done && !s2.done {
-//                return false
+//            if $0.done && !$1.done {
+//               return false
 //            }
-//            if s1.done == s2.done {
-//                return s1.date < s2.date
+//            if $0.done == $1.done {
+//                return $0.date < $1.date
 //            }
 //            
 //            return false
 //        }
+        
+        bucketList.sort { (s1, s2) -> Bool in
+            if !s1.done && s2.done {
+                return true
+            }
+            if s1.done && !s2.done {
+                return false
+            }
+            if s1.done == s2.done {
+                return s1.date < s2.date
+            }
+            
+            return false
+        }
         tableView.reloadData()
         
         
@@ -103,6 +103,13 @@ class BucketItemTableViewController: UITableViewController {
         dateFormatter.dateStyle = .short
         let convertedDate = dateFormatter.string(from: bucketItem.date)
         cell.dateLabel.text = convertedDate
+        
+        if bucketItem.done {
+            cell.backgroundColor = .lightGray
+        }
+        else {
+            cell.backgroundColor = .clear
+        }
 
 
 
@@ -116,15 +123,42 @@ class BucketItemTableViewController: UITableViewController {
         
         let done = UITableViewRowAction(style: .normal, title: "Done") { action, index in
      
-            let alertController = UIAlertController(title: self.bucketList[editActionsForRowAt.row].name, message:
-                "wow u did it, v proud", preferredStyle: UIAlertControllerStyle.alert)
-            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+//            let alertController = UIAlertController(title: self.bucketList[editActionsForRowAt.row].name, message:
+//                "wow u did it, v proud", preferredStyle: UIAlertControllerStyle.alert)
+//            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+//            
+//            
+//            
+//            self.present(alertController, animated: true, completion: nil)
+            self.currentCell = tableView.cellForRow(at: editActionsForRowAt)!
+            self.currentItem = self.bucketList[editActionsForRowAt.row]
             
             
-            
-            self.present(alertController, animated: true, completion: nil)
             self.currentItem?.done = true
+            print(self.currentItem?.done)
+            //self.currentCell?.backgroundColor = UIColor.lightGray
+//
             tableView.reloadData()
+            
+            self.bucketList.sort { (s1, s2) -> Bool in
+                                if !s1.done && s2.done {
+                                    return true
+                                }
+                                if s1.done && !s2.done {
+                                    return false
+                                }
+                                if s1.done == s2.done {
+                                    return s1.date < s2.date
+                                }
+                                
+                                return false
+                            }
+            for row in 0 ..< tableView.numberOfRows(inSection: 0) {
+                //var cell:BucketItemTableViewCell = tableView.cellForRowAt(NSIndexPath(row: row, section: 0) as IndexPath)
+                //var cell: BucketItemTableViewCell = tableView.cellForRow(at: NSIndexPath(row: row, section: 0) as IndexPath)
+                
+                print(row)
+            }
             
         }
         done.backgroundColor = .green
